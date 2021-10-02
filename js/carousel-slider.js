3872805a56800e0567c9d473169d8dde219f1324
Daniel Santos <dados.dan.santos@gmail.com>
2021-10-02 14:05:00 -0300

    feat: adjust banner image position

---
+++ js/carousel-slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoWidth: true,
+        nav: true,
+        lazyLoad: true,
+        autoplay: true,
+        center: true,
+  });
+});
