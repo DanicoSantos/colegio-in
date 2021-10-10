435516c0c778bc8e5fda0010fa9331c31943fe0a
Daniel Santos <dados.dan.santos@gmail.com>
2021-10-09 21:23:25 -0300

    feat: add mob banner to home page

--- js/carousel-slider.js
+++ js/carousel-slider.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoWidth: true,
-        nav: true,
-        lazyLoad: true,
-        autoplay: true,
-        center: true,
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoWidth: true,
+    nav: true,
+    lazyLoad: true,
+    autoplay: true,
+    center: true,
+    slideTransition: `ease`,
+    smartSpeed: 500,
+    autoplaySpeed: 3000,
+    merge: true,
   });
 });
